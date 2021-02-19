f71ef727cd13034a2a9b2c0e0e3edd4a534ef78d
Shaun <614027488@qq.com>
2021-02-19 20:21:59 +0800

    Add files via upload
    图像边缘效果不好
    正在优化linebrush 现在编译可能不可用
    circlebrush能用 但是跟标准答案有差别

---
+++ CircleBrush.cpp
@@ -0,0 +1,54 @@
+#include "impressionistDoc.h"
+#include "impressionistUI.h"
+#include "circlebrush.h"
+#include "math.h"
+
+extern float frand();
+
+CircleBrush::CircleBrush(ImpressionistDoc* pDoc, char* name) :
+	ImpBrush(pDoc, name)
+{
+}
+
+void CircleBrush::BrushBegin(const Point source, const Point target)
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	int size = pDoc->getSize();
+	GLdouble PI = 3.14;
+	GLdouble R = size / 2;
+	GLint n = 32;
+
+	glBegin(GL_POLYGON);
+	SetColor(source);
+	for (int i = 0; i < n; i++)
+		glVertex2d(target.x+R*cos(2*PI*i/n),target.y+R*sin(2*PI*i/n));
+	glEnd();
+
+	//glPointSize((float)size);
+
+	//BrushMove(source, target);
+}
+
+void CircleBrush::BrushMove(const Point source, const Point target)
+{/*
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	if (pDoc == NULL) {
+		printf("CircleBrush::BrushMove  document is NULL\n");
+		return;
+	}
+
+	glBegin(GL_LINES);
+	SetColor(source);
+
+	glVertex2d(target.x, target.y);
+
+	glEnd();*/
+	BrushBegin(source, target);
+}
+
+void CircleBrush::BrushEnd(const Point source, const Point target) {}
+
