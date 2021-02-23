e03be5ee937a551eb8b05843df78a1c6dc8fd4b8
Junxian_Luke_Lu <32172402+Luke-lujunxian@users.noreply.github.com>
2021-02-23 21:20:11 +0800

    Update ImpBrush.cpp

--- ImpBrush.cpp
+++ ImpBrush.cpp
@@ -46,10 +46,10 @@ void ImpBrush::SetColor (const Point source)
 	ImpressionistDoc* pDoc = GetDocument();
 
 
-	GLubyte color[3];
+	GLubyte color[4] = {0,0,0,pDoc->m_pUI->getBrushAlpha()*255};
 
 	memcpy ( color, pDoc->GetOriginalPixel( source ), 3 );
  
-	glColor3ubv( color );
+	glColor4ubv( color );
 
 }
